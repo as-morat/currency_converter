985ead1bcf67be99f52b5b90beda0d5d39863706
AS MORAT <morat9511@gmail.com>
2024-11-08 01:33:14 +0600

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'Pages/currency_converter_material_page.dart';
+
 void main(){
-  runApp(
-      const MaterialApp(
-        
-  ));
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material_Page(),
+    );
+  }
+
 }
 
